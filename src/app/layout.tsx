588bc6388a077ef701e0309c43aa659cd8fdc060
takumines <takumines1@gmail.com>
2025-02-25 23:45:25 +0900

    add session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/header'
+import { AuthProvider } from '@/lib/auth-providers'
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -15,8 +16,10 @@ export default function RootLayout({
 	return (
 		<html lang="ja">
 			<body>
-				<Header />
-				<main>{children}</main>
+				<AuthProvider>
+					<Header />
+					<main>{children}</main>
+				</AuthProvider>
 			</body>
 		</html>
 	)
